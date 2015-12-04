cd56d966f93772f5d96cdf1cc0befd978e9d5782
Oleg Pavlenko <olegliverpoolpavlenko@yandex.ua>
2015-12-04 19:12:54 +0200

    Setup & styled basic functionctionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  resources :posts
+ resources :projects
  get 'welcome/index' #Defining the route
  root 'welcome#index' #Setting the welocme contoller index action to the root of our app
+
 end
